38d2d964a1442d495771c17c9ed724c812068284
enriquejh <enriquejjoaquinh@gmail.com>
2024-12-16 00:43:44 -0600

    Create playRound() function to play a single round
    This functions takes the user's choice and the computer's choice, and
    compares them to determine the winner (if any) of the round. A message
    is shown on the console, and the scores are updated accordingly. The
    functions that get the choices were updated so that they display the
    selection made

--- js/app.js
+++ js/app.js
@@ -25,6 +25,8 @@ function getComputerChoice() {
             break;
     }
 
+    console.log(`Computer's choice: ${choice}`);
+
     return choice;
 }
 
@@ -40,8 +42,43 @@ function getComputerChoice() {
 function getHumanChoice() {
     let choice = prompt('Type your choice (rock, paper, or scissors): ');
 
+    console.log(`Your choice: ${choice}`)
+
     return choice.toLowerCase();
 }
 
 let computerScore = 0;
 let humanScore = 0;
+
+/*  playRound() takes the human and computer choices, selects the result of
+        the round, and logs a message in the console
+    
+    Pseudocode:
+    - Given the human and computer choices, compares them as follows:
+        - If human is rock and computer scissors, human is paper and computer rock,
+          or human scissors and computer paper, then human won
+        - If human is the same as computer, then nobody won
+        - Else, the computer won
+    - For each case, log a string in the console showing the round results
+*/
+
+function playRound(humanChoice, computerChoice) {
+    const case1 = humanChoice == 'rock' && computerChoice == 'scissors';
+    const case2 = humanChoice == 'paper' && computerChoice == 'rock';
+    const case3 = humanChoice == 'scissors' && computerChoice == 'paper';
+
+    if (case1 || case2 || case3){
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        ++humanScore;
+    } else if (humanChoice == computerChoice){
+        console.log(`It's a draw! Both of you chose ${humanChoice}`);
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        ++computerScore;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
