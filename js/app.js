1fc758557f81659fef2fcdd9e59fcc2f3193b814
enriquejh <enriquejjoaquinh@gmail.com>
2024-12-16 00:08:56 -0600

    Create getHumanChoice() function
    This function asks the user to enter a string between 'rock', 'paper',
    and 'scissors', and returns the user's input in lowercase

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 /*  getComputerChoice() will return randomly one of the following strings:
         "rock", "paper", or "scissors"
-    Create a random number
-
-    Pseudocode:
-    Multiply that number three times
-    Round the number down
-    Depending on the number value, assign the result a different string
-    Return that string
+        
+        Pseudocode:
+    - Create a random number
+    - Multiply that number three times
+    - Round the number down
+    - Depending on the number value, assign the result a different string
+    - Return that string
 */
-
+   
 function getComputerChoice() {
     let randNum = Math.ceil(3 * Math.random());
-
+    
     let choice = "";
     switch (randNum){
         case 1:
@@ -26,4 +26,19 @@ function getComputerChoice() {
     }
 
     return choice;
+}
+
+/*  getHumanChoice() asks the user to enter one of the valid options:
+        "rock", "paper", or "scissors"
+
+    Pseudocode:
+    - Show a message in the console, so the user knows what to type
+    - Retrieve the user's answer and save it into a variable
+    - Return that variable
+*/
+
+function getHumanChoice() {
+    let choice = prompt('Type your choice (rock, paper, or scissors): ');
+
+    return choice.toLowerCase();
 }
